3fcb1acebae8343e89655374f251251362b7b346
Michael Matiu <michael.matiu@eurac.edu>
2020-06-12 12:10:33 +0000

    some temporary code

--- R/temp.R
+++ R/temp.R
@@ -176,6 +176,103 @@ vis.gam(gm3_scaled, plot.type = "contour")
 # -> need to scale if combine latlon with elev
 
 
+# smi piemonte vda --------------------------------------------------------
+
+
+sf_meta_hs <- sf::st_as_sf(join_meta_hs[startsWith(provider, "IT")], 
+                           coords = c("Longitude", "Latitude"), crs = sf::st_crs(4326))
+sf_meta_hs$provider2 <- sf_meta_hs$provider == "IT_SMI"
+mapview::mapview(sf_meta_hs, zcol = "provider2")
+
+
+meta_it_vda$Name %>% sort
+meta_it_piemonte$Name %>% sort
+
+smi_duplicated <- fread("/mnt/CEPH_PROJECTS/ALPINE_WIDE_SNOW/01_MERGING/SMI/duplicates_smi_piemonte.txt")
+meta_it_piemonte[Name %in% smi_duplicated$Piedmont]
+smi_duplicated[!Piedmont %in% meta_it_piemonte$Name]
+meta_it_piemonte$Name %>% sort()
+
+meta_it_smi_hn[Name %in% smi_duplicated$SMI]
+meta_it_smi_hs[Name %in% smi_duplicated$SMI]
+
+smi_duplicated[! SMI %in% meta_it_smi_hn$Name]
+
+meta_it_smi_hn[Name == "Lago_Codelago_Devero"]
+meta_it_smi_hn[startsWith(Name, "Lago_Codelago")]
+meta_it_smi_hn[startsWith(Name, "Lago_")]
+
+join_data_hn[provider == "IT_PIEMONTE" & variable %in% smi_duplicated$Piedmont, .N, variable]
+join_data_hs[provider == "IT_PIEMONTE" & variable %in% smi_duplicated$Piedmont, .N, variable]
+
+
+
+# stns meta == data -------------------------------------------------------
+
+stns_data_hn <- unique(join_data_hn$variable)
+missing_meta_hn <- sort(stns_data_hn[! stns_data_hn %in% join_meta_hn$Name])
+missing_meta_hn_new <- sort(join_meta_hn[provider == "CH_METEOSWISS" & grepl("na", Name), Name])
+
+names(missing_meta_hn_new) <- missing_meta_hn
+
+join_data_hn[variable %in% missing_meta_hn, .N, .(variable, provider)]
+
+# join_meta_hn[Name %in% stns_data_hn]
+
+cbind(sort(missing_meta_hn),
+      sort(join_meta_hn[provider == "CH_METEOSWISS" & grepl("na", Name), Name]))
+
+
+# some plot backup --------------------------------------------------------
+
+
+dt_year0_nonan[mw_start_year == 1980 & qprob == 0.5 & snow == "HS"] %>% 
+  ggplot(aes(Elevation, estimate, shape = country))+
+  geom_hline(yintercept = 0)+
+  # geom_point(size = 0.5)+
+  # geom_smooth(se = F)+
+  geom_bin2d(bins = 50)+
+  scale_fill_viridis_c(trans = "log10")+
+  coord_cartesian(ylim = c(-5, 5))+
+  # facet_grid(country ~ month_f)+
+  facet_wrap(~month_f, nrow = 2)+
+  theme_bw()+
+  xlab("Elevation [m]")+
+  ylab("Trend mean monthly HS [cm/year]")+
+  ggtitle("Linear trend (1980-2000) mean monthly HS")
+
+
+
+# check cv gapfill --------------------------------------------------------
+
+dd <- readRDS("/mnt/CEPH_PROJECTS/ALPINE_WIDE_SNOW/04_GAPFILL/rds/cv-01-1day.rds")
+dd
+dd[, .N, Name]
+dd[is.na(i_rep)]
+dd[!is.na(i_rep),
+   .(mae = mean(abs(value_true - value_fill))), 
+   .(month, Name)] %>% 
+  dcast(Name ~ month)
+
+
+# cv gapfill 5 days ----------------------------------------------------------
+
+run_na <- sum_run(!is.na(mat_1[, i_stn]), k = 5, na_pad = T)
+
+i_row_possible <- which(run_na == 5 & month(vec_dates) %in% c(11, 12, 1:5))
+
+if(length(i_row_possible) == 0) return(NULL)
+
+if(length(i_row_possible) < 100){
+  i_row_sample <- i_row_possible
+} else {
+  i_row_sample <- sample(i_row_possible, 100)
+}
+
+
+vec_dates[i_row_sample[1] - 4:0]
+
+mat_1[i_row_sample[1] - 4:0, i_stn]
 
 # EOF ---------------------------------------------------------------------
 
