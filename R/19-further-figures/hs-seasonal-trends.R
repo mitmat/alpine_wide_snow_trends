8ae739dd737f50e9881b46d751e5c123c5073f46
Michael Matiu <michaelmatiu@gmail.com>
2023-07-10 16:08:16 +0200

    summary figures iugg

--- R/19-further-figures/hs-seasonal-trends.R
+++ R/19-further-figures/hs-seasonal-trends.R
@@ -78,6 +78,58 @@ ggsave(gg,
        height = 5)
 
 
+# collapsed two zones -----------------------------------------------------
+
+
+
+dat_plot_full[, cluster_fct2 := fct_collapse(
+  cluster_fct,
+  North = c("NW", "NE", "North & high Alpine"),
+  South = c("South & high Alpine", "SE")
+)]
+
+gg <-
+  # dat_plot_full[variable %in% c("meanHS_DJF", "meanHS_MAM")] %>% 
+  dat_plot_full[variable %in% c("meanHS_DJF")] %>% 
+  ggplot(aes(estimate*10, Elevation, xmin = est_low*10, xmax = est_high*10, colour = cluster_fct))+
+  geom_vline(xintercept = 0)+
+  geom_point(size = 0.5)+
+  geom_linerange(alpha = 0.3)+
+  facet_grid(. ~ cluster_fct2)+ # free_x or free
+  scale_color_brewer("Region", palette = "Set1")+
+  theme_bw(16)+
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "bottom")+ # 
+  xlab("Linear trend in meanHS_DJF [cm per decade]")+
+  ylab("Elevation [m]")
+
+
+ggsave(gg,
+       filename = "fig/hs-seasonal-trends-DJF.png",
+       width = 12,
+       height = 6)
+
+
+gg <-
+  # dat_plot_full[variable %in% c("meanHS_DJF", "meanHS_MAM")] %>% 
+  dat_plot_full[variable %in% c("meanHS_MAM")] %>% 
+  ggplot(aes(estimate*10, Elevation, xmin = est_low*10, xmax = est_high*10, colour = cluster_fct))+
+  geom_vline(xintercept = 0)+
+  geom_point(size = 0.5)+
+  geom_linerange(alpha = 0.3)+
+  facet_grid(. ~ cluster_fct2)+ # free_x or free
+  scale_color_brewer("Region", palette = "Set1")+
+  theme_bw(16)+
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "bottom")+ # 
+  xlab("Linear trend in meanHS_MAM [cm per decade]")+
+  ylab("Elevation [m]")
+
+
+ggsave(gg,
+       filename = "fig/hs-seasonal-trends-MAM.png",
+       width = 12,
+       height = 6)
 
 # mean and max HS (relative) ------------------------------------------------------
 
@@ -97,32 +149,106 @@ dat_ylim[, max_elev := c(1250, 1250, 3000, 3000, 1250)]
 # remove high relative
 dat_plot_full2[abs(trend.rel*10) > 1]
 dat_plot_full2_sub <- dat_plot_full2[abs(trend.rel*10) < 1]
+dat_plot_full2_sub <- dat_plot_full2
 
 
-# gg <-
-  dat_plot_full2_sub[variable %in% c("meanHS_DJF", "meanHS_MAM")] %>% 
-  ggplot(aes(trend.rel*10, Elevation))+
+dat_plot_full2_sub[, cluster_fct2 := fct_collapse(
+  cluster_fct,
+  North = c("NW", "NE", "North & high Alpine"),
+  South = c("South & high Alpine", "SE")
+)]
+
+gg <-
+  dat_plot_full2_sub[variable %in% c("meanHS_DJF")] %>% 
+  ggplot(aes(trend.rel*10, Elevation, colour = cluster_fct))+
   geom_vline(xintercept = 0)+
-  geom_point(size = 0.1)+
-  # geom_pointrange(size = 0.1, fatten = 0.5)+
-  # geom_jitter(width = 0, height = 0.3)+
-  # facet_wrap(~month_fct, nrow = 2)+
-  facet_grid(. ~ variable_fct, scales = "free", space = "free")+ # free_x or free
-  theme_bw()+
-  theme(panel.grid.minor = element_blank())+
-  # scale_color_brewer("", palette = "Set1", guide = F)+
-  # scale_x_continuous(breaks = 10*seq(-4, 4, by = 2))+
-    scale_x_continuous(labels = scales::percent_format())+
+  geom_point(size = 1)+
+  facet_grid(. ~ cluster_fct2)+ # free_x or free
+  theme_bw(16)+
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "bottom")+
+  scale_color_brewer("", palette = "Set1")+
+  scale_x_continuous(labels = scales::percent_format())+
   scale_y_continuous(breaks = seq(0, 3000, by = 500), limits = c(0, NA))+
-  # geom_blank(inherit.aes = F, data = dat_ylim, aes(x = 0, y = max_elev))+
-  xlab("Linear trend in seasonal HS indices [% per decade]")+
+  xlab("Linear trend in meanHS_DJF [% per decade]")+
   ylab("Elevation [m]")
 
 
-# ggsave(gg,
-#        filename = "/mnt/CEPH_PROJECTS/ALPINE_WIDE_SNOW/PAPER/02_review/fig/zenodo-SM/trends-seasonalHS-GLS-relative.png",
-#        width = 10,
-#        height = 11)
+ggsave(gg,
+       filename = "fig/hs-seasonal-trends-DJF-rel.png",
+       width = 12,
+       height = 6)
 
 
 
+gg <-
+  dat_plot_full2_sub[variable %in% c("meanHS_MAM")] %>% 
+  ggplot(aes(trend.rel*10, Elevation, colour = cluster_fct))+
+  geom_vline(xintercept = 0)+
+  geom_point(size = 1)+
+  facet_grid(. ~ cluster_fct2)+ # free_x or free
+  theme_bw(16)+
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "bottom")+
+  scale_color_brewer("", palette = "Set1")+
+  scale_x_continuous(labels = scales::percent_format(),
+                     limits = c(NA, .2), oob = scales::oob_squish)+
+  scale_y_continuous(breaks = seq(0, 3000, by = 500), limits = c(0, NA))+
+  xlab("Linear trend in meanHS_MAM [% per decade]")+
+  ylab("Elevation [m]")
+
+
+ggsave(gg,
+       filename = "fig/hs-seasonal-trends-MAM-rel.png",
+       width = 12,
+       height = 6)
+
+
+
+
+# by meanHS instead of elev -----------------------------------------------
+
+load("~/alps-snow/ALPINE_WIDE_SNOW/PAPER/02_review/rds/data4corr-01-merged.rda")
+# need to adjust by lapse rate!
+dat_elev_eobs <- readRDS("~/alps-snow/ALPINE_WIDE_SNOW/08_AUX_GRIDS/extract-stn/eobs-02-elev.rds")
+
+dat_hs_apgd_eobs2 <- merge(dat_hs_apgd_eobs, dat_elev_eobs, by = "Name") %>% 
+  merge(dat_meta_clust)
+dat_hs_apgd_eobs2[, tmean_lapse := tmean + (elev_eobs - Elevation) * 6.5 / 1000]
+
+
+mitmatmisc::add_hydro_year(dat_hs_apgd_eobs2)
+mitmatmisc::add_season_fct(dat_hs_apgd_eobs2)
+
+dat_hs_apgd_eobs2[hydro_year %in% c(1981:2010),
+                  .(HS = mean(HS),
+                    tmean_lapse = mean(tmean_lapse),
+                    prec = sum(prec),
+                    nn = .N),
+                  .(Name, hydro_year, season)] %>% 
+  .[nn == 3] %>% 
+  .[, 
+    .(HS = mean(HS),
+      tmean_lapse = mean(tmean_lapse),
+      prec = mean(prec),
+      nn = .N),
+    .(Name, season)] %>% 
+  .[nn == 28] -> dat_clim
+
+
+
+
+dat_plot_full2_sub[variable %in% c("meanHS_DJF")] %>% 
+  merge(dat_clim[season == "DJF"]) %>% 
+  ggplot(aes(trend.rel*10, HS, colour = cluster_fct))+
+  geom_vline(xintercept = 0)+
+  geom_point(size = 1)+
+  facet_grid(. ~ cluster_fct2)+ # free_x or free
+  theme_bw(16)+
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "bottom")+
+  scale_color_brewer("", palette = "Set1")+
+  scale_x_continuous(labels = scales::percent_format())+
+  # scale_y_continuous(breaks = seq(0, 3000, by = 500), limits = c(0, NA))+
+  xlab("Linear trend in meanHS_DJF [% per decade]")+
+  ylab("Elevation [m]")
